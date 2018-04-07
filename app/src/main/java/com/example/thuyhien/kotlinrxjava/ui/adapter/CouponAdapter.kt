813bbc5edc8b44d7198e6943e42001e3eb92b98e
Tai.DangVan <tai.dangvan@2359media.com>
2018-04-07 17:50:29 +0700

    correct click listener

--- app/src/main/java/com/example/thuyhien/kotlinrxjava/ui/adapter/CouponAdapter.kt
+++ app/src/main/java/com/example/thuyhien/kotlinrxjava/ui/adapter/CouponAdapter.kt
@@ -1,9 +1,9 @@
 package com.example.thuyhien.kotlinrxjava.ui.adapter
 
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.thuyhien.kotlinrxjava.R
+import com.example.thuyhien.kotlinrxjava.extensions.inflateView
 import com.example.thuyhien.kotlinrxjava.model.Coupon
 import com.example.thuyhien.kotlinrxjava.ui.listener.CouponsActivityListener
 import com.example.thuyhien.kotlinrxjava.ui.viewholder.CouponViewHolder
@@ -11,19 +11,19 @@ import com.example.thuyhien.kotlinrxjava.ui.viewholder.CouponViewHolder
 /**
  * Created by thuyhien on 3/21/18.
  */
-class CouponAdapter(private val couponList : List<Coupon>, private val couponsActivityListener: CouponsActivityListener)
-    : RecyclerView.Adapter<CouponViewHolder>() {
+class CouponAdapter(private val couponList: List<Coupon>, private val couponsActivityListener: CouponsActivityListener) : RecyclerView.Adapter<CouponViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CouponViewHolder {
-        val rowView = LayoutInflater.from(parent?.context)
-                .inflate(R.layout.item_store_coupon, parent, false)
-        return CouponViewHolder(rowView, couponsActivityListener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CouponViewHolder {
+        val vh = CouponViewHolder(parent.inflateView(R.layout.item_store_coupon))
+        vh.itemView.setOnClickListener {
+            val coupon = couponList[vh.adapterPosition]
+            couponsActivityListener.onClickCoupon(coupon)
+        }
+        return vh
     }
+
+    override fun onBindViewHolder(holder: CouponViewHolder, position: Int) = holder.bind(couponList[position])
 
     override fun getItemCount() = couponList.size
-
-    override fun onBindViewHolder(holder: CouponViewHolder?, position: Int) {
-        holder?.bind(couponList[position])
-    }
 
 }
--- app/src/main/java/com/example/thuyhien/kotlinrxjava/ui/viewholder/CouponViewHolder.kt
+++ app/src/main/java/com/example/thuyhien/kotlinrxjava/ui/viewholder/CouponViewHolder.kt
@@ -1,34 +1,19 @@
 package com.example.thuyhien.kotlinrxjava.ui.viewholder
 
-import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.example.thuyhien.kotlinrxjava.model.Coupon
-import com.example.thuyhien.kotlinrxjava.ui.listener.CouponsActivityListener
-import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_store_coupon.*
-import java.lang.ref.WeakReference
 
 /**
  * Created by thuyhien on 3/21/18.
  */
-class CouponViewHolder(containerView : View, couponsActivityListener: CouponsActivityListener)
-    : BaseViewHolder<Coupon>(containerView) {
-    private var listenerWeakRef = WeakReference<CouponsActivityListener>(couponsActivityListener)
-    private lateinit var coupon : Coupon
-
-    init {
-        containerView.setOnClickListener({
-            getCouponActivityListener()?.onClickCoupon(coupon)
-        })
-    }
+class CouponViewHolder(itemView: View) : BaseViewHolder<Coupon>(itemView) {
 
     override fun bind(data: Coupon) {
-        coupon = data
         tvStore.text = data.store
         tvInfo.text = data.coupon
         tvDate.text = data.expiryDate
         tvCode.text = data.couponCode
     }
-
-    private fun getCouponActivityListener() = listenerWeakRef.get()
+    
 }
