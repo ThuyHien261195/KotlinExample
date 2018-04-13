72d50b552423b62528efce419f5455a6d98adc59
ThuyHien261195 <thuyhien261195@gmail.com>
2018-04-13 11:03:21 +0700

    Add service class

---
+++ app/src/main/java/com/example/thuyhien/kotlinrxjava/service/BoundService.kt
@@ -0,0 +1,14 @@
+package com.example.thuyhien.kotlinrxjava.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+/**
+ * Created by thuyhien on 4/13/18.
+ */
+class BoundService : Service() {
+    override fun onBind(intent: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/example/thuyhien/kotlinrxjava/service/PlayerInService.kt
@@ -0,0 +1,58 @@
+package com.example.thuyhien.kotlinrxjava.service
+
+import android.app.Service
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.IBinder
+import android.util.Log
+import com.example.thuyhien.kotlinrxjava.R
+import com.example.thuyhien.kotlinrxjava.ui.activity.ExampleServiceActivity.Companion.TAG_FILE_PATH
+import java.io.IOException
+
+/**
+ * Created by thuyhien on 4/12/18.
+ */
+class PlayerInService : Service() {
+
+    lateinit var filePath: String
+    companion object {
+        lateinit var mediaPlayer: MediaPlayer
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.reset()
+        Log.e("2359", "On create service")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        getData(intent!!)
+        initMediaListener()
+        Log.e("2359", "On start command")
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+
+    private fun getData(intent: Intent) {
+        filePath = intent.getStringExtra(TAG_FILE_PATH)
+    }
+
+    private fun initMediaListener() {
+        try {
+            mediaPlayer = MediaPlayer.create(this, R.raw.example_song)
+        } catch (e : Exception) {
+            Log.e("2359", "Cannot set data source ${e.message}")
+        }
+        mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener { mp ->  }
+    }
+}
